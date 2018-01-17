38edcf8c3b7146753c869c937ff029de7d56610d
FARID 1972 <FARID 1972@ndafrds>
2018-01-17 11:39:09 +0800

    Project9v2

---
+++ src/project9/Project9.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project9;
+
+/**
+ *
+ * @author FARID 1972
+ */
+public class Project9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/test1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FARID 1972
+ */
+public class test1 {
+    
+public static void main(String args[]) {
+int x =30;
+if
+( x <20) {
+    System.out.print("This is if statement");
+}else{
+    System.out.print("This is if statment");
+}
+    }
+}
+        
+      
+
